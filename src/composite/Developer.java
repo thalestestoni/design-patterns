3f9b272c0aa717bdf558a61957c765fd8ed1702a
Thales Flores Testoni <thales.testoni@havan.com.br>
2022-05-10 21:48:38 -0300

    Composite

---
+++ src/composite/Developer.java
@@ -0,0 +1,7 @@
+package composite;
+
+public class Developer extends CompositeEmployee {
+    public Developer(int codigo, String nome, String cargo) {
+        super(codigo, nome, cargo);
+    }
+}
